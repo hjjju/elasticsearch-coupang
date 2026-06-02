3a023ad35a488eb436f7ac9d07f5542212ab9c6e
hjjju <yujjuu22@gmail.com>
2026-06-02 17:11:18 +0900

    coupang elasticsearch

---
+++ src/main/java/com/example/coupangapiserver/product/ProductService.java
@@ -0,0 +1,92 @@
+package com.example.coupangapiserver.product;
+
+import co.elastic.clients.elasticsearch._types.query_dsl.MultiMatchQuery;
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
+import co.elastic.clients.elasticsearch._types.query_dsl.TextQueryType;
+import com.example.coupangapiserver.product.domain.Product;
+import com.example.coupangapiserver.product.domain.ProductDocument;
+import com.example.coupangapiserver.product.dto.CreateProductRequestDto;
+import java.util.List;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.client.elc.NativeQuery;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Service
+public class ProductService {
+
+  private final ProductRepository productRepository;
+
+  private final ProductDocumentRepository productDocumentRepository;
+
+  private final ElasticsearchOperations elasticsearchOperations;
+
+  public ProductService(ProductRepository productRepository, ProductDocumentRepository productDocumentRepository, ElasticsearchOperations elasticsearchOperations) {
+    this.productRepository = productRepository;
+    this.productDocumentRepository = productDocumentRepository;
+    this.elasticsearchOperations = elasticsearchOperations;
+  }
+
+  public List<Product> getProducts(int page, int size) {
+    Pageable pageable = PageRequest.of(page - 1, size);
+    return productRepository.findAll(pageable).getContent();
+  }
+
+  public Product createProduct(CreateProductRequestDto createProductRequestDto) {
+    Product product = new Product(
+        createProductRequestDto.getName(),
+        createProductRequestDto.getDescription(),
+        createProductRequestDto.getPrice(),
+        createProductRequestDto.getRating(),
+        createProductRequestDto.getCategory()
+    );
+    Product savedProduct =  productRepository.save(product);
+
+    ProductDocument productDocument = new ProductDocument(
+            savedProduct.getId().toString(),
+            savedProduct.getName(),
+            savedProduct.getDescription(),
+            savedProduct.getPrice(),
+            savedProduct.getRating(),
+            savedProduct.getCategory()
+    );
+
+    productDocumentRepository.save(productDocument);
+
+    return savedProduct;
+
+  }
+
+  public void deleteProduct(Long id) {
+    productRepository.deleteById(id);
+    productDocumentRepository.deleteById(id.toString());
+  }
+
+  public List<String> getSuggestions(String query) {
+    Query multiMatchQuery = MultiMatchQuery.of(m -> m
+            .query(query)
+            .type(TextQueryType.BoolPrefix)
+            .fields("name.auto_complete", "name.auto_complete.2gram", "name.auto_complete.3gram")
+    )._toQuery();
+
+    NativeQuery nativeQuery = NativeQuery.builder()
+            .withQuery(multiMatchQuery)
+            .withPageable(PageRequest.of(0, 5))
+            .build();
+
+    SearchHits<ProductDocument> searchHits = this.elasticsearchOperations.search(nativeQuery, ProductDocument.class);
+    return searchHits.getSearchHits().stream()
+            .map(hit -> {
+              ProductDocument productDocument = hit.getContent();
+              System.out.println(productDocument); //"_source" 안에 들어있는 정보
+              return productDocument.getName();
+            })
+            .toList();
+  }
+}
